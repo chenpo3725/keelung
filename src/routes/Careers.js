4c4331cf55b7b61a3510ddddcf43c0ee9b2906e7
chenpo <chenpo3725@gmail.com>
2025-10-19 18:14:55 +0800

    env

--- src/routes/Careers.js
+++ src/routes/Careers.js
@@ -1,4 +1,39 @@
+import { useState } from "react";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import jwtDecode from 'jwt-decode';
+
+const clientId = process.env.VITE_GOOGLE_CLIENT_ID;
 const Careers = () => {
-  return <h1>Careers Page</h1>;
-};
+    const [authData, setAuthData] = useState(null);
+
+     const gContainer = {
+       display: 'flex',
+       flexDirection: 'column',
+       alignItems: 'center',
+       marginTop: '50px'
+     };
+
+return (
+       <GoogleOAuthProvider clientId={clientId}>
+         <div style={gContainer}>
+           <GoogleLogin
+             onSuccess={(credentialResponse) => {
+               setAuthData(credentialResponse);
+               console.log("Success!", credentialResponse);
+             }}
+             onError={() => {
+               console.log("Login Failed");
+             }}
+           />
+           {authData && (
+             <div>
+               <p>Credential: {authData.credential}</p>
+               <p>Select By: {authData.select_by}</p>
+             </div>
+           )}
+         </div>
+       </GoogleOAuthProvider>
+     );
+   }
+
 export default Careers;
